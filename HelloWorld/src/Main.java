ed58fb57a526f94425a2023f9137ae1a1480f6cd
Nekto13 <deform@tut.by>
2018-02-23 19:52:51 +0300

    second commit

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World Again!");
+        System.out.println("Эта строчка написана лишь для того, что бы проверить работу GIT.");
     }
 }
