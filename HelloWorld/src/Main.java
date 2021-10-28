dfee832a16c84116c613e139967500348ab3da98
Siarhei Stasiulevich <deform@tut.by>
2021-10-28 10:44:28 +0300

    Update class Main

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,6 +1,11 @@
+import entity.Printer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World Again!");
-        System.out.println("Эта строчка написана лишь для того, что бы проверить работу GIT.");
+        //System.out.println("Hello World Again!");
+        //System.out.println("Эта строчка написана лишь для того, что бы проверить работу GIT.");
+        Printer printer = new Printer();
+
+        printer.print("New message from first branch");
     }
 }
